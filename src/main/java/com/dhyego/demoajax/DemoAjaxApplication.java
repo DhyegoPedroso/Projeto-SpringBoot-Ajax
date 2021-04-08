816718b7426cd0fa5c3f979a6bb9be60e75cbebc
Dhyego Pedroso <dhyego.pedroso@outlook.com>
2021-04-08 14:56:35 -0300

    Metodo getSocialMetaTagByUrl Criado

--- src/main/java/com/dhyego/demoajax/DemoAjaxApplication.java
+++ src/main/java/com/dhyego/demoajax/DemoAjaxApplication.java
@@ -1,11 +1,12 @@
 package com.dhyego.demoajax;
 
-import com.dhyego.demoajax.domain.SocialMetaTag;
-import com.dhyego.demoajax.service.SocialMetaTagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.dhyego.demoajax.domain.SocialMetaTag;
+import com.dhyego.demoajax.service.SocialMetaTagService;
 
 @SpringBootApplication
 public class DemoAjaxApplication implements CommandLineRunner {
@@ -20,15 +21,11 @@ public class DemoAjaxApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        SocialMetaTag og = service
-                .getOpenGraphByUrl("https://www.pichau.com.br/hardware/placa-m-e/placa-mae-asus-rog-strix-z490-f-gaming-ddr4-socket-lga1200-intel-z490");
-        System.out.println(og);
-
-        System.out.println("\n");
-        
-        SocialMetaTag twitter = service
-                .getTwitterCardByUrl("https://www.pichau.com.br/hardware/placa-m-e/placa-mae-asus-rog-strix-z490-f-gaming-ddr4-socket-lga1200-intel-z490");
-        System.out.println(twitter);
+        System.out.println("");
+        System.out.println("");
+        SocialMetaTag tag = service.getSocialMetaTagByUrl(
+                "https://www.terabyteshop.com.br/produto/15153/fonte-gamdias-kratos-e1-500w-add-rgb-80-plus-gd-z500zzz");
+        System.out.println(tag.toString());
 
     }
 
--- src/main/java/com/dhyego/demoajax/service/SocialMetaTagService.java
+++ src/main/java/com/dhyego/demoajax/service/SocialMetaTagService.java
@@ -1,37 +1,50 @@
 package com.dhyego.demoajax.service;
 
-import com.dhyego.demoajax.domain.SocialMetaTag;
 import java.io.IOException;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import com.dhyego.demoajax.domain.SocialMetaTag;
+
 /**
- * 
+ *
  * @author Dhyego Pedroso
  */
-
 @Service
 public class SocialMetaTagService {
-    
-    public SocialMetaTag getOpenGraphByUrl (String url){
-        
-        SocialMetaTag tag = new SocialMetaTag();
-        try {
-            Document doc = Jsoup.connect(url).get();
-            tag.setTitle(doc.head().select("meta[property=og:title]").attr("content"));
-            tag.setSite(doc.head().select("meta[property=og:site_name]").attr("content"));
-            tag.setImage(doc.head().select("meta[property=og:image]").attr("content"));
-            tag.setUrl(doc.head().select("meta[property=og:url]").attr("content"));
-        } catch (IOException ex) {
-          ex.printStackTrace();
+
+    private static Logger log = LoggerFactory.getLogger(SocialMetaTagService.class);
+
+    public SocialMetaTag getSocialMetaTagByUrl(String url) {
+
+        SocialMetaTag twitter = getTwitterCardByUrl(url);
+        SocialMetaTag openGraph = getOpenGraphByUrl(url);
+
+        if (!isEmpty(openGraph)) {
+            return openGraph;
         }
-        
-        return tag;
+
+        if (!isEmpty(twitter)) {
+            return twitter;
+
+        } else {
+
+            SocialMetaTag tag = new SocialMetaTag();
+            tag.setTitle("null");
+            tag.setSite("null");
+            tag.setImage("null");
+            tag.setUrl("null");
+            return tag;
+
+        }
+
     }
-    
-    public SocialMetaTag getTwitterCardByUrl (String url){
-        
+
+    private SocialMetaTag getTwitterCardByUrl(String url) {
         SocialMetaTag tag = new SocialMetaTag();
         try {
             Document doc = Jsoup.connect(url).get();
@@ -39,11 +52,39 @@ public class SocialMetaTagService {
             tag.setSite(doc.head().select("meta[name=twitter:site]").attr("content"));
             tag.setImage(doc.head().select("meta[name=twitter:image]").attr("content"));
             tag.setUrl(doc.head().select("meta[name=twitter:url]").attr("content"));
-        } catch (IOException ex) {
-          ex.printStackTrace();
+        } catch (IOException e) {
+            log.error(e.getMessage(), e.getCause());
         }
-        
         return tag;
     }
 
+    private SocialMetaTag getOpenGraphByUrl(String url) {
+        SocialMetaTag tag = new SocialMetaTag();
+        try {
+            Document doc = Jsoup.connect(url).get();
+            tag.setTitle(doc.head().select("meta[property=og:title]").attr("content"));
+            tag.setSite(doc.head().select("meta[property=og:site_name]").attr("content"));
+            tag.setImage(doc.head().select("meta[property=og:image]").attr("content"));
+            tag.setUrl(doc.head().select("meta[property=og:url]").attr("content"));
+        } catch (IOException e) {
+            log.error(e.getMessage(), e.getCause());
+        }
+        return tag;
+    }
+
+    private boolean isEmpty(SocialMetaTag tag) {
+        if (tag.getImage().isEmpty()) {
+            return true;
+        }
+//        if (tag.getSite().isEmpty()) {
+//            return true;
+//        }
+        if (tag.getTitle().isEmpty()) {
+            return true;
+        }
+//        if (tag.getUrl().isEmpty()) {
+//            return true;
+//        }
+        return false;
+    }
 }
