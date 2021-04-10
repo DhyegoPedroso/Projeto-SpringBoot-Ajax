e45ebcc854f2474ef1da11cdda6e400a9238ca76
Dhyego Pedroso <dhyego.pedroso@outlook.com>
2021-04-10 11:42:14 -0300

    Limpando os dados da solicitação anterior

--- src/main/resources/static/js/promo-add.js
+++ src/main/resources/static/js/promo-add.js
@@ -7,22 +7,34 @@ $("#linkPromocao").on('change', function () {
 
         $.ajax({
             method: "POST",
-            url:"/meta/info?url=" + url,
+            url: "/meta/info?url=" + url,
             cache: false,
-            success: function ( data ){
+
+            beforeSend: function () {
+                $("#alert").removeClass("alert alert-danger").text('');
+                $("#titulo").val('');
+                $("#site").text('');
+                $("#linkImagem").attr("src", '/images/promo-dark.png');
+            },
+
+            success: function (data) {
                 console.log(data);
                 $("#titulo").val(data.title);
-                $("#site").text(data.site.replace("@",""));
-                $("#linkImagem").attr("src",data.image);
+                $("#site").text(data.site.replace("@", ""));
+                $("#linkImagem").attr("src", data.image);
             },
+
             statusCode: {
+
                 404: function () {
                     $("#alert").addClass("alert alert-danger").text("Nenhuma informação pode ser recuperada dessa URL.");
                 }
             },
+
             error: function () {
-                  $("#alert").addClass("alert alert-danger").text("Ops... algo deu errado, tente mais tarde.");
+                $("#alert").addClass("alert alert-danger").text("Ops... algo deu errado, tente mais tarde.");
             }
+
         })
     }
 });
