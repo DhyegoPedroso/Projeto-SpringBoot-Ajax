5bbe3f22462bb88539e2bb46a60a398942478532
Dhyego Pedroso <dhyego.pedroso@outlook.com>
2021-04-12 11:11:03 -0300

    Classe Promocao Criada

---
+++ src/main/java/com/dhyego/demoajax/domain/Promocao.java
@@ -0,0 +1,124 @@
+package com.dhyego.demoajax.domain;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.springframework.format.annotation.NumberFormat;
+
+/**
+ *
+ * @author Dhyego Pedroso
+ */
+@Entity
+@Table(name = "promocao")
+public class Promocao implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "titulo", nullable = false)
+    private String titulo;
+
+    @Column(name = "link_promocao", nullable = false)
+    private String linkPromocao;
+
+    @Column(name = "site_promocao", nullable = false)
+    private String site;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    @Column(name = "link_imagem", nullable = false)
+    private String linkImagem;
+
+    @NumberFormat(style = NumberFormat.Style.CURRENCY, pattern = "#,##0.00")
+    @Column(name = "preco_promocao", nullable = false)
+    private BigDecimal preco;
+
+    @Column(name = "total_likes")
+    private int likes;
+
+    @Column(name = "data_cadastro", nullable = false)
+    private LocalDate dtCadastro;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getLinkPromocao() {
+        return linkPromocao;
+    }
+
+    public void setLinkPromocao(String linkPromocao) {
+        this.linkPromocao = linkPromocao;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getLinkImagem() {
+        return linkImagem;
+    }
+
+    public void setLinkImagem(String linkImagem) {
+        this.linkImagem = linkImagem;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public LocalDate getDtCadastro() {
+        return dtCadastro;
+    }
+
+    public void setDtCadastro(LocalDate dtCadastro) {
+        this.dtCadastro = dtCadastro;
+    }
+
+}
