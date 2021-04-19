c27df36ee73204f12454280f18899fcf738ae04c
Dhyego Pedroso <dhyego.pedroso@outlook.com>
2021-04-19 17:32:57 -0300

    Infinite Scroll em ação

--- src/main/resources/static/js/promo-list.js
+++ src/main/resources/static/js/promo-list.js
@@ -1,36 +1,58 @@
 var pageNumber = 0;
 
-// efeito infinite scroll
+$(document).ready(function () {
+    $("#loader-img").hide();
+    $("#fim-btn").hide();
+});
+
+// efeito infinte scroll
 $(window).scroll(function () {
-   
-   var scrollTop = $(this).scrollTop();
-   var conteudo = $(document).height() - $(window).height();
-   
-    console.log('scroll: ', scrollTop, ' | ', 'conteudo', conteudo);
-    
+
+    var scrollTop = $(this).scrollTop();
+    var conteudo = $(document).height() - $(window).height();    
+
     if (scrollTop >= conteudo) {
-        
-       pageNumber++;
-       
+        pageNumber++;
         setTimeout(function () {
             loadByScrollBar(pageNumber);
         }, 200);
     }
-    
+
 });
 
 function loadByScrollBar(pageNumber) {
-    
+
     $.ajax({
-       
-       method: "GET",
-       url: "/promocao/list/ajax",
-       data: {
-           page: pageNumber
-       },
-       
-        success: function (response) {
-            console.log("resposta > ", response);
+        method: "GET",
+        url: "/promocao/list/ajax",
+        data: {
+            page: pageNumber
+        },
+        
+        beforeSend: function () {
+            $("#loader-img").show();
+        },
+        
+        success: function (response) {   
+
+            if (response.length > 150) {
+
+                $(".row").fadeIn(250, function () {
+                    $(this).append(response);
+                });
+
+            } else {
+                $("#fim-btn").show();
+                $("#loader-img").removeClass("loader");
+            }            
+        },
+        
+        error: function (xhr) {
+            alert("Ops, ocorreu um erro: " + xhr.status + " - " + xhr.statusText);
+        },
+        
+        complete: function () {
+            $("#loader-img").hide();
         }
         
     });
