c944e0db521461859f7a893a376ef754f9d0756b
Dhyego Pedroso <dhyego.pedroso@outlook.com>
2021-04-20 10:37:21 -0300

    O método Ajax para likes

--- src/main/resources/static/js/promo-list.js
+++ src/main/resources/static/js/promo-list.js
@@ -7,61 +7,75 @@ $(document).ready(function () {
 
 // efeito infinte scroll
 $(window).scroll(function () {
-    
+
     var scrollTop = $(this).scrollTop();
-    var conteudo = $(document).height() - $(window).height();    
-    
+    var conteudo = $(document).height() - $(window).height();
+
     if (scrollTop >= conteudo) {
         pageNumber++;
         setTimeout(function () {
             loadByScrollBar(pageNumber);
         }, 200);
     }
-    
+
 });
 
 function loadByScrollBar(pageNumber) {
-    
+
     $.ajax({
         method: "GET",
         url: "/promocao/list/ajax",
         data: {
             page: pageNumber
         },
-        
+
         beforeSend: function () {
             $("#loader-img").show();
         },
-        
-        success: function (response) {   
-            
+
+        success: function (response) {
+
             if (response.length > 150) {
-                
+
                 $(".row").fadeIn(250, function () {
                     $(this).append(response);
                 });
-                
+
             } else {
                 $("#fim-btn").show();
                 $("#loader-img").removeClass("loader");
-            }            
+            }
         },
-        
+
         error: function (xhr) {
             alert("Ops, ocorreu um erro: " + xhr.status + " - " + xhr.statusText);
         },
-        
+
         complete: function () {
             $("#loader-img").hide();
         }
-        
+
     });
-    
+
 }
 
 // adicionar likes
-$("button[id*='likes-btn-']").on("click", function () {
+$(document).on("click", "button[id*='likes-btn-']", function () {
     var id = $(this).attr("id").split("-")[2];
-    
+
     console.log("id: ", id);
+
+    $.ajax({
+        method: "POST",
+        url: "/promocao/like" + id,
+
+        success: function (response) {
+            $("#link-count-" + id).text(response);
+        },
+
+        error: function (xhr) {
+            alert("Ops!! ocorreu um erro: " + xhr.status + ", " + xhr.statusText);
+        }
+
+    });
 });
