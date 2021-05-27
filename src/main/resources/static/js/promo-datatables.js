e332db718331072bc99ae7bdde352b4841b55a31
Dhyego Pedroso <dhyego.pedroso@outlook.com>
2021-05-26 21:02:05 -0300

    Finalizando o modal de update

--- src/main/resources/static/js/promo-datatables.js
+++ src/main/resources/static/js/promo-datatables.js
@@ -1,19 +1,19 @@
 $(document).ready(function () {
-
+    
     moment.locale('pt-br');
-
+    
     var table = $("#table-server").DataTable({
-
+        
         processing: true,
         serverSide: true,
         responsive: true,
         lengthMenu: [10, 15, 20, 25],
-
+        
         ajax: {
             url: "/promocao/datatable/server",
             data: "data"
         },
-
+        
         columns: [
             {data: 'id'},
             {data: 'titulo'},
@@ -25,46 +25,43 @@ $(document).ready(function () {
             {data: 'likes'},
             {data: 'dtCadastro', render:
                         function (dtCadastro) {
-                            return moment(dtCadastro).format('LLLL');
-                        }},
+                            return moment(dtCadastro).format('LLL');
+                        }
+            },
             {data: 'categoria.titulo'}
         ],
-
+        
         dom: 'Bfrtip',
-
+        
         buttons: [
-
             {
                 text: 'Editar',
                 attr: {
                     id: 'btn-editar',
                     type: 'button'
                 },
-
                 enabled: false
             },
-
+            
             {
                 text: 'Excluir',
                 attr: {
                     id: 'btn-excluir',
                     type: 'button'
                 },
-
                 enabled: false
             }
-
         ]
-
     });
 
-    // acao para marcar/desmarcar botoes ao clicar na ordenacao
+    // acao para marcar/desmarcar botoes ao clicar na ordenacao 
     $("#table-server thead").on('click', 'tr', function () {
         table.buttons().disable();
     });
 
-    // acao para marcar/desmarcar linhas clicadas
+    // acao para marcar/desmarcar linhas clicadas 
     $("#table-server tbody").on('click', 'tr', function () {
+        
         if ($(this).hasClass('selected')) {
             $(this).removeClass('selected');
             table.buttons().disable();
@@ -73,46 +70,54 @@ $(document).ready(function () {
             $(this).addClass('selected');
             table.buttons().enable();
         }
+        
     });
 
-    // acao do botao de editar (Abrir Modal)
+    // acao do botao de editar (abrir modal)
     $("#btn-editar").on('click', function () {
+        
         if (isSelectedRow()) {
 
             var id = getPromoId();
-
             $.ajax({
+                
                 method: "GET",
                 url: "/promocao/edit/" + id,
-
-                beforeSend: function (xhr) {
+                
+                beforeSend: function () {
+                    // removendo as mensagens
+                    $("span").closest('.error-span').remove();
+                    //remover as bordas vermelhas
+                    $(".is-invalid").removeClass("is-invalid");
+                    // abre o modal
                     $("#modal-form").modal('show');
                 },
-
+                
                 success: function (data) {
                     $("#edt_id").val(data.id);
                     $("#edt_site").text(data.site);
                     $("#edt_titulo").val(data.titulo);
                     $("#edt_descricao").val(data.descricao);
-                    $("#edt_preco").val(data.preco.toLocaleString('pt-BR', {minimumFractionDigits: 2, maximumFractionDigits: 2}));
+                    $("#edt_preco").val(data.preco.toLocaleString('pt-BR', {
+                        minimumFractionDigits: 2,
+                        maximumFractionDigits: 2
+                    }));
                     $("#edt_categoria").val(data.categoria.id);
                     $("#edt_linkImagem").val(data.linkImagem);
-                    $("#edt_imagem").val('src', data.linkImagem);
+                    $("#edt_imagem").attr('src', data.linkImagem);
                 },
-
+                
                 error: function () {
-                    alert("Ops!... Ocorreu um erro, tente novamente mais tarde.");
+                    alert("Ops... algum erro ocorreu, tente novamente.");
                 }
+                
             });
-
-
         }
-
     });
 
     // submit do formulario para editar
     $("#btn-edit-modal").on("click", function () {
-
+        
         var promo = {};
         promo.descricao = $("#edt_descricao").val();
         promo.preco = $("#edt_preco").val();
@@ -122,24 +127,24 @@ $(document).ready(function () {
         promo.id = $("#edt_id").val();
 
         $.ajax({
-
+            
             method: "POST",
             url: "/promocao/edit",
             data: promo,
-
+            
             beforeSend: function () {
                 // removendo as mensagens
                 $("span").closest('.error-span').remove();
                 //remover as bordas vermelhas
                 $(".is-invalid").removeClass("is-invalid");
             },
-
+            
             success: function () {
                 $("#modal-form").modal("hide");
                 table.buttons().disable();
                 table.ajax.reload();
             },
-
+            
             statusCode: {
                 422: function (xhr) {
                     console.log('status error:', xhr.status);
@@ -152,7 +157,7 @@ $(document).ready(function () {
                     });
                 }
             }
-
+            
         });
     });
 
@@ -162,7 +167,7 @@ $(document).ready(function () {
         $("#edt_imagem").attr("src", link);
     });
 
-    // acao do botao de excluir (Abrir Modal)
+    // acao do botao de excluir (abrir modal)
     $("#btn-excluir").on('click', function () {
         if (isSelectedRow()) {
             $("#modal-delete").modal('show');
@@ -171,33 +176,32 @@ $(document).ready(function () {
 
     // exclusao de uma promocao
     $("#btn-del-modal").on('click', function () {
-
+        
         var id = getPromoId();
         $.ajax({
+            
             method: "GET",
             url: "/promocao/delete/" + id,
-
+            
             success: function () {
                 $("#modal-delete").modal('hide');
+                table.buttons().disable();
                 table.ajax.reload();
             },
-
+            
             error: function () {
-                alert("Ops!... Ocorreu um erro, tente novamente mais tarde.");
+                alert("Ops... Ocorreu um erro, tente mais tarde.");
             }
+            
         });
-
     });
 
-    // funcao que pega o id da linha selecionada
     function getPromoId() {
         return table.row(table.$('tr.selected')).data().id;
     }
 
-    // funcao para vereficar se alguma linha foi selecionada
     function isSelectedRow() {
         var trow = table.row(table.$('tr.selected'));
         return trow.data() !== undefined;
     }
-
 });
